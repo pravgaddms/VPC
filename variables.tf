a7eadda25f8288857815d17c110160360c581b16
gpreddyjgl <34214246+gpreddyjgl@users.noreply.github.com>
2020-08-22 09:30:26 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet1_cidr" {
+  default     = "10.0.0.0/24"
+}
+
+variable "subnet1_az" {
+  default     = "us-east-1a"
+}
+variable "region" {
+  default = "us-east-1"
+}
